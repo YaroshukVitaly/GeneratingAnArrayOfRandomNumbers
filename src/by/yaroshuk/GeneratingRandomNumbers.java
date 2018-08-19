81cf1fca139632274466db261d9dded8e105d0c3
Vitalya <yaroshukvi@gmail.com>
2018-08-19 17:57:35 +0300

    Поиск минимального значения

--- src/by/yaroshuk/GeneratingRandomNumbers.java
+++ src/by/yaroshuk/GeneratingRandomNumbers.java
@@ -9,7 +9,10 @@ public class GeneratingRandomNumbers {
 
     public static void main(String[] args) {
         int[] array = generateRandomIntArray(24, 100);
+        int min = findMinimum(array);
+
         System.out.println(Arrays.toString(array));
+        System.out.println("min = " + min);
 
 
     }
@@ -21,5 +24,14 @@ public class GeneratingRandomNumbers {
             array [i] = random.nextInt(upperBound);
         }
         return array;
+    }
+    private static int findMinimum(int[] array){
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]){
+                min = array[i];
+            }
+        }
+        return min;
     }
 }

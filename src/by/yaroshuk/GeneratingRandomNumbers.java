c430d1c4d6fc40d2f2ef33cb904fa3424d5df8be
Vitalya <yaroshukvi@gmail.com>
2018-08-19 17:48:57 +0300

    Создание через метод

--- src/by/yaroshuk/GeneratingRandomNumbers.java
+++ src/by/yaroshuk/GeneratingRandomNumbers.java
@@ -8,13 +8,18 @@ import java.util.Random;
 public class GeneratingRandomNumbers {
 
     public static void main(String[] args) {
-       int [] array = new int[10];
-        Random random = new Random();
-        for (int i = 0; i < array.length ; i++) {
-            array [i] = random.nextInt(100);
-        }
+        int[] array = generateRandomIntArray(24, 100);
         System.out.println(Arrays.toString(array));
 
 
+    }
+
+    private static int[] generateRandomIntArray(int arrayLength, int upperBound) {
+        int [] array = new int[arrayLength];
+        Random random = new Random();
+        for (int i = 0; i < array.length ; i++) {
+            array [i] = random.nextInt(upperBound);
+        }
+        return array;
     }
 }
